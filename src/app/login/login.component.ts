d80ac3f06d74ebfa911a7f027099c6940c8364b1
Yassine-123-mdb <134618428+Yassine-123-mdb@users.noreply.github.com>
2024-12-09 00:22:28 +0100

    login commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,23 +14,8 @@ export class LoginComponent {
   err: number = 0; // Pour la gestion des erreurs
 
   constructor(private authService: AuthService, private router: Router) {}
-  onLoggedin() {
-    this.authService.login(this.user).subscribe({
-      next: (response) => {
-        
-          this.message = 'Login réussi.';
-          this.authService.redirectUser() // Par exemple
-        
-      },
-      error: (err) => {
-        this.message = 'Erreur lors de la connexion.';
-        this.err = 1;
-      }
-    });
-  }
-  
 
-  /* onLoggedin() {
+  onLoggedin() {
     this.authService.login(this.user).subscribe({
       next: (response) => {
         this.authService.handleLoginResponse(response); // Délègue la logique au service
@@ -45,5 +30,5 @@ export class LoginComponent {
         }
       },
     });
-  } */
+  }
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -38,33 +38,19 @@ public regitredUser : User = new User();
   validateEmail(code : string){
     return this.http.get<User>(this.apiURL+'/verifyEmail/'+code);
     }
-    login(user: User) {
-      return this.http.post<User>(this.apiURL + '/login', user, { observe: 'response' }).pipe(
-        tap((response) => {
-          // Assurez-vous d'extraire et de gérer l'utilisateur et son statut 'enabled' ici
-          if (response.body?.enabled === false) {
-            // Ici vous pouvez afficher le message de désactivation
-            console.log('Utilisateur désactivé');
-          } else {
-            // Si l'utilisateur est activé, vous pouvez rediriger ou gérer l'état
-            this.redirectUser();
-          }
-        })
-      );
-    }
-    
+    login(user : User)
+  {
+    return this.http.post<User>(this.apiURL+'/login', user , {observe:'response'});
+  
+  }
     isRole(role: string): boolean {
       return this.roles?.includes(role) ?? false;
     }
     
     handleLoginResponse(response: any) {
-      const jwtToken = response.headers.get('Authorization');
-      if (jwtToken) {
-        this.saveToken(jwtToken); // Sauvegarder et décoder le JWT
+       // Sauvegarder et décoder le JWT
         this.redirectUser(); // Redirection après avoir décodé les rôles
-      } else {
-        console.error("Erreur : le token JWT n'a pas été reçu.");
-      }
+      
     }
     
     redirectUser() {
@@ -163,8 +149,4 @@ public regitredUser : User = new User();
     });
   }*/
     
-}
-
-function tap(arg0: (response: any) => void): import("rxjs").OperatorFunction<import("@angular/common/http").HttpResponse<User>, unknown> {
-  throw new Error('Function not implemented.');
 }
